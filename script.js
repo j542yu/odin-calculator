c5ede1b986f4cfe1462696909c5b8e3b73d6a3c7
judywsl <j542yu@uwaterloo.ca>
2025-01-07 13:33:43 -0500

    Add basic operator functions and html buttons

---
+++ script.js
@@ -0,0 +1,28 @@
+function add(a, b) {
+    return Number(a) + Number(b);
+}
+
+function subtract(a, b) {
+    return Number(a) - Number(b);
+}
+
+function multiply(a, b) {
+    return Number(a) * Number(b);
+}
+
+function divide(dividend, divisor) {
+    dividend = Number(dividend);
+    divisor = Number(divisor);
+
+    if (divisor !== 0) {
+        return dividend/divisor;  
+    }
+}
+
+let operator = null;
+let operandOne = null;
+let operandTwo = null;
+
+function operate(operandOne, operator, operandTwo) {
+    return operator(operandOne, operandTwo);
+}
