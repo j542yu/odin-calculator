00f2244034be94932cfaa1815a22ee871a20f732
judywsl <j542yu@uwaterloo.ca>
2025-01-07 14:12:42 -0500

    Add real-time display showing number clicked
    Allows user to see what they are currently inputting

--- script.js
+++ script.js
@@ -26,3 +26,15 @@ let operandTwo = null;
 function operate(operandOne, operator, operandTwo) {
     return operator(operandOne, operandTwo);
 }
+
+const allOperands = document.querySelectorAll(".operand");
+
+allOperands.forEach((button) => button.addEventListener("click", () => populateCurrentDisplay(button)));
+
+function populateCurrentDisplay(input) {
+    if (typeof input !== "number") {
+        input = Number(input.textContent);
+    }
+    const currentDisplay = document.querySelector(".display .current");
+    currentDisplay.textContent = input;
+}
